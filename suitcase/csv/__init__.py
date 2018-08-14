55709730e0a5f60c264db5e008fc8643d0eb2539
danielballan <daniel.b.allan@gmail.com>
2018-08-14 17:01:37 -0400

    WIP

--- suitcase/csv/__init__.py
+++ suitcase/csv/__init__.py
@@ -10,9 +10,51 @@ del get_versions
 # know which portions of the suitcase API it supports without calling any
 # functions.
 #
-# def export(...)
-#     ...
-#
+from collections import defaultdict
+import json
+
+
+def export(gen, filepath):
+    """
+    Export a stream of documents to CSV files.
+
+    Creates {filepath}_meta.json and then {filepath}_{stream_name}.csv
+    for every Event stream.
+
+    The structure of the json is:
+
+    {'start': {...},
+     'descriptors':
+         {'<stream_name>': [{...}, {...}, ...],
+          ...},
+     'stop': {...}}
+    """
+    meta = {}  # to be exported as JSON at the end
+    meta['descriptors'] = defaultdict(list)  # map stream_name to descriptors
+    files = {}  # map descriptor uid to file handle of CSV file
+    try:
+        for name, doc in gen:
+            if name == 'start':
+                if 'start' in meta:
+                    raise RuntimeError("This exporter expects documents from one "
+                                    "run only.")
+                meta['start'] = doc
+            elif name == 'stop':
+                meta['stop'] = doc
+            elif name == 'descriptor':
+                stream_name = doc.get('name')
+                meta['descriptors'][stream_name].append(doc)
+                filepath = f"{filepath}_{stream_name}_{doc['uid'][:8]}.csv"
+                files[doc['uid']] = open(filepath, 'w')
+            elif name == 'event':
+                row = ', '.join(map(str, (doc['time'], *doc['data'].values())))
+                f = files[doc['descriptor']]
+                f.write(row)
+    finally:
+        for f in files.values():
+            f.close()
+    with open(f"{filepath}_meta.json", 'w') as f:
+        json.dump(meta, f)
 #
 # def ingest(...):
 #     ...
--- suitcase/csv/tests.py
+++ suitcase/csv/tests.py
@@ -1,2 +1,22 @@
 # Tests should generate (and then clean up) any files they need for testing. No
 # binary files should be included in the repository.
+from bluesky.tests.conftest import RE
+from ophyd.tests.conftest import hw
+from bluesky.plans import count
+import tempfile
+from . import export
+
+
+def test_export(RE, hw):
+    collector = []
+
+    def collect(name, doc):
+        collector.append((name, doc))
+
+    RE.subscribe(collect)
+    RE(count([hw.det]))
+
+    with tempfile.NamedTemporaryFile(mode='w+') as f:
+        export(collector, f.name)
+        f.seek(0)
+        print(f.read())
