bce0ff353ad0a5078ffecc42424d9b41cbb1e765
Dan Allan <dallan@bnl.gov>
2018-09-19 09:33:49 -0400

    TST: A basic test is now working.

--- tests/utils.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import numpy as np
-
-def generate_csv(fh, num_rows=1, num_columns=1, delimiter=','):
-    """Writes a csv file to the given file handle.
-    """
-    df = pandas.DataFrame(np.random.randn(num_rows, num_columns))
-    df.to_csv(path_or_buf=fh, sep=delimiter)
