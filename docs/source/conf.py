630efa6ccbd9fd8580b775ef1b7dace09aaf5e98
Andrew Walter <awalter@bnl.gov>
2019-02-07 14:35:26 -0500

    FIX: module name in dos config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,17 +67,17 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'suitcase.csv'
-copyright = 'suitcase.csv Contributors'
-author = 'suitcase.csv Contributors'
+project = 'suitcase.Csv'
+copyright = 'suitcase.Csv Contributors'
+author = 'suitcase.Csv Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
-import suitcase.csv
+import suitcase.Csv
 # The short X.Y version.
-version = suitcase.csv.__version__
+version = suitcase.Csv.__version__
 # The full version, including alpha/beta/rc tags.
 release = version
 
